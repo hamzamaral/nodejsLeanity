069bf0576f10a4067bea06a59e14de4ca03950fa
hamza_maral <mhamamaralmhm.65@gmail.com>
2025-09-24 13:42:54 +0300

    17. Html Sayfalarının Getirilmesi

--- blogapp/index.js
+++ blogapp/index.js
@@ -2,20 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/blogs/:blogid/users/:username", function(req, res) {
-    console.log(req.params.blogid);
-    console.log(req.params.username);
-    res.send("blog detay sayfası");
+const path = require("path");
+// Bu modül, kodunuzun farklı işletim sistemlerinde
+//  (Windows, macOS, Linux) sorunsuz çalışması için kritik öneme sahiptir.
+
+app.use("/blogs/:blogid", function(req, res) {
+    console.log(__dirname);
+    console.log(__filename);
+    res.sendFile(path.join(__dirname, "views/users","blog-details.html"));
 });
 
 app.use("/blogs", function(req, res) {
-    res.send("blog listesi");
+    res.sendFile(path.join(__dirname, "views/users","blogs.html"));
 });
 
 app.use("/", function(req, res) {
-    res.send("anasayfa");
+    res.sendFile(path.join(__dirname, "views/users","index.html"));
 });
-
 
 app.listen(3000, function() {
     console.log("listening on port 3000");
