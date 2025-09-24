e4780c5e2237f5beb1ff22751ecf4c99951155ac
hamza_maral <mhamamaralmhm.65@gmail.com>
2025-09-24 16:19:16 +0300

    27. Partial View' e Veri Gönderme

--- blogapp/routes/user.js
+++ blogapp/routes/user.js
@@ -10,51 +10,32 @@ const data = {
             baslik: "Komple Uygulamalı Web Geliştirme",
             aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
             resim: "1.jpeg",
-            anasayfa: true
+            anasayfa: true,
+            onay: true
         },
         {
             blogid: 2,
             baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
             aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
             resim: "2.jpeg",
-            anasayfa: true
+            anasayfa: true,
+            onay: false
         },
         {
             blogid: 3,
             baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
             aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
             resim: "3.jpeg",
-            anasayfa: false
-        },        {
+            anasayfa: false,
+            onay: true
+        },
+        {
             blogid: 3,
             baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
             aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
             resim: "3.jpeg",
-            anasayfa: false
-        },        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false
-        },        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false
-        },        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false
-        },        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false
+            anasayfa: false,
+            onay: true
         },
     ]
 }
