a0e75bdc5d2a7ff580edf26d84a371839b4e21cb
hamza_maral <mhamamaralmhm.65@gmail.com>
2025-09-24 10:39:55 +0300

    9. File System

--- website/app.js
+++ website/app.js
@@ -1,53 +1,27 @@
 var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer((request, response) => {
     
     if(request.url == '/') {
-        response.writeHead(200, {"Content-Type": "text/html"});
-
-        response.write(`
-            <html>
-                <head>
-                    <title>anasayfa</title>
-                    <meta charset="utf-8">
-                </head>            
-                <body>
-                    <h1>Anasayfa</h1>
-                </body>
-            </html>`);
-        
-        response.end();
+        fs.readFile("index.html", (error, html) => {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(html);        
+            response.end();
+        });        
     }
     else if(request.url == "/blogs") {
-        response.writeHead(200, {"Content-Type": "text/html"});
-
-        response.write(`
-            <html>
-                <head>
-                    <title>blogs</title>
-                    <meta charset="utf-8">
-                </head>            
-                <body>
-                    <h1>blog listesi</h1>
-                </body>
-            </html>`);
-        
-        response.end();
+        fs.readFile("blogs.html", (error, html) => {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(html);        
+            response.end();
+        });   
     } else {
-        response.writeHead(404, {"Content-Type": "text/html"});
-
-        response.write(`
-            <html>
-                <head>
-                    <title>404</title>
-                    <meta charset="utf-8">
-                </head>            
-                <body>
-                    <h1>aradığınız kaynak bulunamadı.</h1>
-                </body>
-            </html>`);
-        
-        response.end();
+        fs.readFile("404.html", (error, html) => {
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.write(html);        
+            response.end();
+        });   
     }
 
 });
