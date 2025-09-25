b67efcfb946d72ef23a635071baca0fa185d70ab
hamza_maral <mhamamaralmhm.65@gmail.com>
2025-09-25 14:58:58 +0300

    81

--- blogapp/index.js
+++ blogapp/index.js
@@ -5,11 +5,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-
 // app.use(express.urlencoded({ extended: false }));: Bu bir "middleware"dir. HTML formlarından (<form>)
 //      gönderilen verileri (kullanıcı adı, şifre, blog başlığı vb.) ayrıştırıp req.body nesnesi
 //       içinde erişilebilir hale getirir. Bu satır olmadan form verilerini alamazsınız.
-
 
 const path = require("path");
 const userRoutes = require("./routes/user");
@@ -19,21 +17,21 @@ app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
-app.use(userRoutes); 
+app.use(userRoutes);
 
 const sequelize = require("./data/db");
 const dummyData = require("./data/dummy-data");
 const Category = require("./models/category");
 const Blog = require("./models/blog");
 
-Blog.belongsToMany(Category, { through: "blogCategories"});
-Category.belongsToMany(Blog, { through: "blogCategories"});
+Blog.belongsToMany(Category, { through: "blogCategories" });
+Category.belongsToMany(Blog, { through: "blogCategories" });
 
 (async () => {
-    await sequelize.sync({ force: true });
-    await dummyData();
+  await sequelize.sync({ force: true });
+  await dummyData();
 })();
 
-app.listen(3000, function() {
-    console.log("listening on port 3000");
+app.listen(3000, function () {
+  console.log("listening on port 3000");
 });
