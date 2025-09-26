08adea7ff76ccc57048c56e708e5ad8657d977b7
hamza_maral <mhamamaralmhm.65@gmail.com>
2025-09-26 16:38:22 +0300

    140. İlişkili Veriler Üzerinde Güncelleme ve Silme

--- shopapp/app.js
+++ shopapp/app.js
@@ -9,25 +9,30 @@ const home = require("./routes/home");
 
 app.use(express.json());
 
-app.use("/api/products" ,products);
-app.use("/api/categories" ,categories);
+app.use("/api/products", products);
+app.use("/api/categories", categories);
 app.use("/", home);
 
-const username = "";
-const password = "";
-const database = "";
+const username = "mhamamaralmhm65_db_user";
+const password = "MasF8vvuSpRDqiYT";
+const database = "nodejsdeneme";
 
 (async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.kfjfak2.mongodb.net/${database}?retryWrites=true&w=majority`);
-        console.log("mongodb bağlantısı kuruldu.");
-    }
-    catch(err) {
-        console.log(err);
-    }
+  try {
+    // mongodb+srv://mhamamaralmhm65_db_user:MasF8vvuSpRDqiYT@cluster0.4veashr.mongodb.net/
+
+    // mongodb+srv://mhamamaralmhm65:<db_password>@cluster0.nkj6cbr.mongodb.net/  MasF8vvuSpRDqiYT
+    await mongoose.connect(
+      `mongodb+srv://${username}:${password}@cluster0.4veashr.mongodb.net/${database}?retryWrites=true&w=majority`
+    );
+    console.log("mongodb bağlantısı kuruldu.");
+  } catch (err) {
+    console.log(err);
+  }
 })();
 
-
 app.listen(3000, () => {
-    console.log("listening on port 3000");
+  console.log("listening on port 3000");
 });
+
+ 
